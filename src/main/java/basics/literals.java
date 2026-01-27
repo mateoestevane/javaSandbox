95e879f53600c396712237a69d0b1deca3ab1a7a
Fernanda Itzel Flores Valenzuela <f376160@uabc.edu.mx>
2026-01-26 22:36:03 -0600

    Creando estructura inicial del proyecto

---
+++ src/main/java/basics/literals.java
@@ -0,0 +1,11 @@
+
+public class literals {
+    public static void main(String[] args) {
+
+        int hola = 20;
+        char uwu = 'x';
+        String saludos = "hola quee tal";
+
+        System.out.println(saludos + " me llamo " + uwu + " tengo " + hola + " años");
+    }
+}
