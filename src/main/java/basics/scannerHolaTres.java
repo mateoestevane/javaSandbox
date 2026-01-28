ea0e43ca4a1d3ccf4966cbc04939788c7a1328b4
Mateo Gómez <mateoestevanegt@gmail.com>
2026-01-28 12:52:14 -0600

    Scanner final, estudiar eso

--- src/main/java/basics/scannerHolaTres.java
+++ src/main/java/basics/scannerHolaTres.java
@@ -1,22 +1,23 @@
 package basics;
 
-
 import java.util.Scanner;
 
 public class scannerHolaTres {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        double peso;
-        double altura;
-        String nombre;
         System.out.println("Introduce tu nombre completo: " );
-        nombre = scanner.nextLine();
+        String nombre = scanner.nextLine();
         System.out.println("Introduce tu peso: ");
-        peso = scanner.nextDouble();
+        double peso = scanner.nextDouble();
         System.out.println("Introduce tu altura: ");
-        altura = scanner.nextDouble();
-        System.out.println("Tu nombre completo es: " + nombre + " y tu IMC es de: " + (peso/Math.pow(altura,2)));
+        double altura = scanner.nextDouble();
+
+        double imc = peso / Math.pow(altura,2);
+        imc = Math.round (imc * 100.0) / 100.0;
+        System.out.println("Tu nombre completo es: " + nombre + " y tu IMC es de: " + imc);
+
+        scanner.close();
 
 
 
