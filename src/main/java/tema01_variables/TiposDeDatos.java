3cc3efe027fd04db4053588fd3e6b95528e50b1e
Mateo Gómez <mateoestevanegt@gmail.com>
2026-02-04 11:50:05 -0600

    Tema 01: Tarea Completada 1 - TiposDeDatos

--- src/main/java/tema01_variables/TiposDeDatos.java
+++ src/main/java/tema01_variables/TiposDeDatos.java
@@ -2,6 +2,18 @@ package tema01_variables;
 
 public class TiposDeDatos {
     public static void main(String[] args) {
+        int edad = 23;
+        double precio = 19.99;
+        String nombre = "Mateo";
+        char inicial = nombre.charAt(0);
+        boolean esEstudiante = true;
+        System.out.println("Mi edad es: " + edad);
+        System.out.printf("El precio es: $%.2f\n",precio);
+        System.out.printf("Mi nombre es: "+ nombre);
+        System.out.println("Tu inicial es: " + inicial);
+        System.out.println("Eres estudiante: " + esEstudiante);
+
+
 
     }
 }
